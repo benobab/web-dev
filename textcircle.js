fde1f1d6eb6bb97c0c9523d7290329e7e5bb49fe
Ben <benlacroix83@gmail.com>
2016-11-04 18:24:50 +0800

    html interpretor / viewer of code tapping live ok

--- textcircle.js
+++ textcircle.js
@@ -9,6 +9,13 @@ if (Meteor.isClient){
             }else{
                 return undefined;
             }
+        },
+        config: function () {
+          return function(editor) {
+            editor.on("change",function(cm_editor,info){
+                $('#viewer_iframe').contents().find("html").html(cm_editor.getValue());
+            });
+          }
         }
     });
 }
