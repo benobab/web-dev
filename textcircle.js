33d6c90a0e9dcae9f4533a04d8d476c4f5e43fe6
Ben <benlacroix83@gmail.com>
2016-11-04 16:27:31 +0800

    create collection + insert one object, and add a package mizzao sharejs-codemirror

--- textcircle.js
+++ textcircle.js
@@ -1,3 +1,5 @@
+this.Documents = new Mongo.Collection("documents");  
+
 if (Meteor.isClient){
 
 }
@@ -5,5 +7,8 @@ if (Meteor.isClient){
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
-	})
+        if(!Documents.findOne()){ //no document yet
+            Documents.insert({title:"my new document"});
+        }
+	});
 }
